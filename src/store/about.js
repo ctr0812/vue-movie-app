24b52cb2ff18c50ad65de20c6a31ad3c22287960
ctr0812 <andy0812@naver.com>
2021-08-06 06:13:03 +0900

    Create project

---
+++ src/store/about.js
@@ -0,0 +1,10 @@
+export default {
+  namespaced: true,
+  state: () => ({
+    name: 'CTR',
+    email: 'andy0812@naver.com',
+    blog: 'https://naver.com',
+    phone: '+82-10-1234-5678',
+    image: 'https://heropy.blog/css/images/logo.png'
+  })
+}
